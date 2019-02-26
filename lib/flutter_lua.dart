f8763e9d52a47f1ce65f921214a662dbd0eac33f
Arto Bendiken <arto@bendiken.net>
2019-02-26 18:08:27 +0200

    Fixed style nits with `flutter format`.

--- lib/flutter_lua.dart
+++ lib/flutter_lua.dart
@@ -5,8 +5,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterLua {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_lua');
+  static const MethodChannel _channel = const MethodChannel('flutter_lua');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
