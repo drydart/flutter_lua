4d309e70e59b0bdef2d1d29019e8836fad51c5d3
Arto Bendiken <arto@bendiken.net>
2019-03-14 11:54:56 +0200

    Implemented Lua.version and LuaThread.spawn() on iOS. (#1)

--- ios/Classes/FlutterLuaPlugin.m
+++ ios/Classes/FlutterLuaPlugin.m
@@ -1,3 +1,5 @@
+/* This is free and unencumbered software released into the public domain. */
+
 #import "FlutterLuaPlugin.h"
 #import <flutter_lua/flutter_lua-Swift.h>
 
--- ios/Classes/SwiftFlutterLuaPlugin.swift
+++ ios/Classes/SwiftFlutterLuaPlugin.swift
@@ -1,14 +1,70 @@
+/* This is free and unencumbered software released into the public domain. */
+
 import Flutter
+import Flutter_lua_vm
 import UIKit
+
+var _threadID: Int = 0
 
 public class SwiftFlutterLuaPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_lua", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterLuaPlugin()
+    let instance = SwiftFlutterLuaPlugin(registrar: registrar)
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  let registrar: FlutterPluginRegistrar
+
+  init(registrar: FlutterPluginRegistrar) {
+    self.registrar = registrar
+    super.init()
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch (call.method) {
+      case "getVersion":
+        result(Flutter_lua_vmVersion())
+
+      case "spawnThread":
+        _threadID += 1
+        FlutterLuaThreadHandler.register(with: registrar)
+        result(_threadID)
+
+      default:
+        result(FlutterMethodNotImplemented)
+    }
+  }
+}
+
+private class FlutterLuaThreadHandler: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_lua/#\(_threadID)", binaryMessenger: registrar.messenger())
+    let instance = FlutterLuaThreadHandler(registrar: registrar, threadID: _threadID)
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  let registrar: FlutterPluginRegistrar
+  let threadID: Int
+
+  init(registrar: FlutterPluginRegistrar, threadID: Int) {
+    self.registrar = registrar
+    self.threadID = threadID
+    super.init()
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch (call.method) {
+      case "evalString":
+        result(FlutterMethodNotImplemented) // TODO
+
+      case "evalAsset":
+        result(FlutterMethodNotImplemented) // TODO
+
+      case "evalFile":
+        result(FlutterMethodNotImplemented) // TODO
+
+      default:
+        result(FlutterMethodNotImplemented)
+    }
   }
 }
